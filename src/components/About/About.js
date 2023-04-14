a8f84fccaf01a5e39c79acc609450674151c9f24
ImAlAmin <alamin.cse97@gmail.com>
2023-04-14 17:00:09 +0600

    added to about

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+           <h1 className='text-2xl text-indigo-600 font-bold text-center mt-12'>Nothing To say .... SORRY 😢</h1>
+        </div>
+    );
+};
+
+export default About;
